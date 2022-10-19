afd1ec1a69ae0f019f8eec993103cba78f4126b5
Matish <matias.altamiranove@gmail.com>
2022-10-19 10:17:08 -0300

    f

---
+++ js_repaso/index.js
@@ -0,0 +1,9 @@
+
+// funciones 
+
+// arrow functions
+const fatFn = () => {
+}
+// a diferencia de las funciones (function) no pueden ser llamadas con la palabra reservada new
+const r1 = fatFn();
+console.log(r1);
